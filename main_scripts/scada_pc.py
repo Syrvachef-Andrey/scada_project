69aa13e039f6154598a07287c3383edb4d6f6f98
Андрей <an.sirvacheff@yandex.ru>
2026-09-07 20:52:36 +0500

    feat: add sliders for all axis

--- main_scripts/scada_pc.py
+++ main_scripts/scada_pc.py
@@ -17,33 +17,47 @@ class ScadaWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("SCADA: Robot Control")
-        self.setGeometry(100, 100, 350, 120)
+        self.setGeometry(500, 500, 350, 350)
 
         self.mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self.mqtt_client.connect(BROKER_ADDRESS, 1883, 60)
         self.mqtt_client.loop_start()
 
+        self.list_of_names_of_angles = ['base', 'shoulder', 'elbow', 'forearm', 'wrist', 'gripper']
+        self.angles = {
+            "base": 90, "shoulder": 90, "elbow": 90,
+            "forearm": 90, "wrist": 90, "gripper": 90
+        }
+
+        self.labels = {}
+
         widget = QWidget()
         layout = QVBoxLayout()
 
-        self.lbl_axis = QLabel("Ось 1 (Base): 90°")
-        layout.addWidget(self.lbl_axis)
+        for i, axis_name in enumerate(self.list_of_names_of_angles):
+            lbl = QLabel(f"Ось {i + 1} ({axis_name}): 90°")
+            self.labels[axis_name] = lbl
+            layout.addWidget(lbl)
 
-        self.slider = QSlider(Qt.Horizontal)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(180)
-        self.slider.setValue(90)
-        self.slider.valueChanged.connect(self.on_slider_change)
-        layout.addWidget(self.slider)
+            slider = QSlider(Qt.Horizontal)
+            slider.setMinimum(0)
+            slider.setMaximum(180)
+            slider.setValue(90)
+
+            slider.valueChanged.connect(
+                lambda val, name=axis_name, idx=i: self.on_slider_change(val, name, idx)
+            )
+            layout.addWidget(slider)
 
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
-    def on_slider_change(self, value):
-        self.lbl_axis.setText(f"Ось 1 (Base): {value}°")
-        # Формируем JSON пакет
-        payload = {"base": value, "shoulder": 90}
-        self.mqtt_client.publish("scada/arm/angles", json.dumps(payload))
+    def on_slider_change(self, value, axis_name, idx):
+        self.labels[axis_name].setText(f"Ось {idx + 1} ({axis_name}): {value}°")
+
+        self.angles[axis_name] = value
+
+        self.mqtt_client.publish("scada/arm/angles", json.dumps(self.angles))
 
 
 if __name__ == "__main__":
